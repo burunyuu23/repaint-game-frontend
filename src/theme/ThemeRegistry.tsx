e59cca97675fa8127242f766de94d177592f3f0d
danilkk <ezzfvkoko@gmail.com>
2023-08-02 23:06:24 +0300

    init @mui theme provider

---
+++ src/theme/ThemeRegistry.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {createTheme, CssBaseline, ThemeOptions, ThemeProvider} from "@mui/material";
+import {NextAppDirEmotionCacheProvider} from "@/theme/EmotionCache";
+import {Roboto} from "next/font/google";
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin']
+})
+
+const themeOptions: ThemeOptions = {
+    typography: {
+        fontFamily: roboto.style.fontFamily,
+        fontSize: 22,
+    },
+    palette: {
+        background: {
+            default: '#eeeeee'
+        }
+    },
+
+}
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({children} : {children: React.ReactNode}) {
+    return (
+        <NextAppDirEmotionCacheProvider options={{ key: 'mui'}}>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+        </NextAppDirEmotionCacheProvider>
+    );
+}
