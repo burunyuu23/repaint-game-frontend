875b511e9f566cfac19542c9051a3c970d765775
danilkk <ezzfvkoko@gmail.com>
2023-08-03 01:07:28 +0300

    refactor: aroundCheck func fix: map size set to 'max-content'

--- src/l3_features/repaint_game/map/map.tsx
+++ src/l3_features/repaint_game/map/map.tsx
@@ -15,57 +15,58 @@ const Map = ({map, fieldSize, colors, mapSize}: Props) => {
 
     const Cells = styled.div`
       grid-template-columns: repeat(${fieldSize}, 1fr);
-      width: ${mapSize};
-      height: ${mapSize};
+      width: max-content;
     `
 
     const Cell = styled.div`
-      width: ${mapSize} / ${fieldSize};
-      height: ${mapSize} / ${fieldSize};
+      width: calc(${mapSize} / ${fieldSize});
+      height: calc(${mapSize} / ${fieldSize});
     `
+
+    function aroundCheck(aroundCell: Cell, curCell: Cell) {
+        return (aroundCell !== undefined && ((aroundCell.captured && !curCell.captured)
+            || (!aroundCell.captured && curCell.captured)
+            || aroundCell.value !== curCell.value))
+    }
 
     function borderClassesAroundCheck(cell: Cell, rowIndex: number, columnIndex: number): string[] {
         const classes: string[] = [];
 
-        if (!cell.captured) {
-            let row = map[rowIndex - 1]
-            if (row !== undefined && (row[columnIndex].captured
-                || row[columnIndex].value !== cell.value)) {
-                classes.push(styles.nonCapturedCellTop)
-            }
-            row = map[rowIndex + 1]
-            if (row !== undefined && row[columnIndex].captured) {
-                classes.push(styles.nonCapturedCellBottom)
-            }
+        let row = map[rowIndex - 1]
+        if (row != undefined && aroundCheck(row[columnIndex], cell)) {
+            classes.push(styles.nonCapturedCellTop)
+        }
+        row = map[rowIndex + 1]
+        if (row != undefined && aroundCheck(row[columnIndex], cell)) {
+            classes.push(styles.nonCapturedCellBottom)
+        }
 
-            row = map[rowIndex]
-            let column = row[columnIndex - 1]
-            if (column !== undefined && (column.captured
-                || column.value !== cell.value)) {
-                classes.push(styles.nonCapturedCellLeft)
-            }
+        row = map[rowIndex]
+        let column = row[columnIndex - 1]
+        if (aroundCheck(column, cell)) {
+            classes.push(styles.nonCapturedCellLeft)
+        }
 
-            column = row[columnIndex + 1]
-            if (column !== undefined && column.captured) {
-                classes.push(styles.nonCapturedCellRight)
-            }
+        column = row[columnIndex + 1]
+        if (aroundCheck(column, cell)) {
+            classes.push(styles.nonCapturedCellRight)
         }
 
         return classes;
     }
 
     return (
-            <Cells className={styles.map}>
-                {map.map(
-                    (cellRow, rowIndex) =>
-                        cellRow.map(
-                            (cell, columnIndex) =>
-                                <Cell
-                                    key={rowIndex * fieldSize + columnIndex}
-                                    className={borderClassesAroundCheck(cell, rowIndex, columnIndex).join(' ')}
-                                    style={{backgroundColor: colors[cell.value].hexCode}}/>
-                        ))}
-            </Cells>
+        <Cells className={styles.map}>
+            {map.map(
+                (cellRow, rowIndex) =>
+                    cellRow.map(
+                        (cell, columnIndex) =>
+                            <Cell
+                                key={rowIndex * fieldSize + columnIndex}
+                                className={borderClassesAroundCheck(cell, rowIndex, columnIndex).join(' ')}
+                                style={{backgroundColor: colors[cell.value].hexCode}}/>
+                    ))}
+        </Cells>
     );
 };
 
