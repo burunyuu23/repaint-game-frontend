a2b28f5d209f0a9f37414f9bd1e16235a0bbdc86
danilkk <ezzfvkoko@gmail.com>
2023-08-04 18:08:37 +0300

    feat: add transitions for animated repaint captured blocks

--- src/l3_features/repaint_game/map/map.tsx
+++ src/l3_features/repaint_game/map/map.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Cell} from "@/l4_entities/repaint-game/models/cell";
 import {Color} from "@/l4_entities/repaint-game/models/color";
 import styles from './map.module.scss';
 import styled from "@emotion/styled";
+import {Transition} from "react-transition-group";
 
 type Props = {
     map: Cell[][],
+    prevMap: Cell[][]
     fieldSize: number,
     onclick: () => void,
     colors: Color[],
-    mapSize: string
+    mapSize: string,
 }
-const Map = ({map, fieldSize, colors, mapSize}: Props) => {
+const Map = ({map, prevMap, fieldSize, colors, mapSize}: Props) => {
 
     const Cells = styled.div`
       grid-template-columns: repeat(${fieldSize}, 1fr);
@@ -22,6 +24,23 @@ const Map = ({map, fieldSize, colors, mapSize}: Props) => {
       width: calc(${mapSize} / ${fieldSize});
       height: calc(${mapSize} / ${fieldSize});
     `
+
+    const defaultStyle = {
+        transition: `background 200ms ease-in`,
+    }
+
+    const transitionStyles = (state: string, prevColor: string, currColor: string) => {
+        switch (state) {
+            case "entering":
+                return {background: prevColor}
+            case "entered":
+                return {background: currColor}
+            case "exiting":
+                return {background: currColor}
+            case "exited":
+                return {background: prevColor}
+        }
+    };
 
     function aroundCheck(aroundCell: Cell, curCell: Cell) {
         return (aroundCell !== undefined && ((aroundCell.captured && !curCell.captured)
@@ -55,18 +74,45 @@ const Map = ({map, fieldSize, colors, mapSize}: Props) => {
         return classes;
     }
 
+    const nodeRef = useRef(null);
+
+    const [entered, setEntered] = useState(false);
+    useEffect(() => {
+        setEntered(true);
+    }, [])
+
     return (
-        <Cells className={styles.map}>
-            {map.map(
-                (cellRow, rowIndex) =>
-                    cellRow.map(
-                        (cell, columnIndex) =>
-                            <Cell
-                                key={rowIndex * fieldSize + columnIndex}
-                                className={borderClassesAroundCheck(cell, rowIndex, columnIndex).join(' ')}
-                                style={{backgroundColor: colors[cell.value].hexCode}}/>
-                    ))}
-        </Cells>
+        <Transition
+            nodeRef={nodeRef}
+            timeout={200}
+            in={entered}
+        >
+            {(state: string) => (
+                <div>
+                    <Cells className={`${styles.map} 
+                `}
+                           ref={nodeRef}>
+                        {map.map(
+                            (cellRow, rowIndex) =>
+                                cellRow.map(
+                                    (cell, columnIndex) =>
+                                        <Cell
+                                            key={rowIndex * fieldSize + columnIndex}
+                                            className={`${borderClassesAroundCheck(cell, rowIndex, columnIndex).join(' ')}
+                                            `}
+                                            style={prevMap[rowIndex][columnIndex].value !== cell.value
+                                                ?
+                                                { ...defaultStyle, ...transitionStyles(state,
+                                                        colors[prevMap[rowIndex][columnIndex].value].hexCode,
+                                                        colors[cell.value].hexCode)}
+                                                :
+                                                {backgroundColor: colors[cell.value].hexCode}}/>
+                                ))}
+                    </Cells>
+                </div>
+            )}
+
+        </Transition>
     );
 };
 
