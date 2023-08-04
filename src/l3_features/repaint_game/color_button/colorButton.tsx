59ffe241ba6e48c7093b256ce5eb5c707a72fe1b
danilkk <ezzfvkoko@gmail.com>
2023-08-04 18:06:11 +0300

    feat: add glowable clickable and children to ColorButton

--- src/l3_features/repaint_game/color_button/colorButton.tsx
+++ src/l3_features/repaint_game/color_button/colorButton.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import {Button} from "@mui/material";
 import styles from './colorButton.module.scss';
+import styled from "@emotion/styled";
 
 type Props = {
     colorHexCode: string,
-    onclick: () => void
+    onclick: () => void,
+    notclickable?: boolean,
+    children?: React.ReactNode,
+    glowable?: boolean
 }
 
-const ColorButton = ({colorHexCode, onclick}: Props) => {
+const ColorButton = ({colorHexCode, onclick, notclickable, children, glowable}: Props) => {
 
     return (
-            <Button onClick={onclick}
-                    className={styles.colorButton}
-                    style={{backgroundColor: colorHexCode}}>
+            <Button
+                onClick={notclickable ? undefined: onclick}
+                    className={`${notclickable ? styles.colorButton : styles.clickableColorButton}`}
+                    style={
+                        glowable === true ? {backgroundColor: colorHexCode,
+                        boxShadow: `0 0 10px 5px ${colorHexCode}`} :
+                            {backgroundColor: colorHexCode,
+                                boxShadow: `0 0 20px ${colorHexCode}`}}>
+                <div className={styles.children}>{children}</div>
             </Button>
     );
 }
