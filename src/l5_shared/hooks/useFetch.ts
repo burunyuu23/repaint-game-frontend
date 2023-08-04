1f8e15f1bbeeacdce5c92072d619cb18309ceb68
danilkk <ezzfvkoko@gmail.com>
2023-08-04 18:09:33 +0300

    feat: add clearError()

--- src/l5_shared/hooks/useFetch.ts
+++ src/l5_shared/hooks/useFetch.ts
@@ -1,18 +1,25 @@
-import {useState} from "react";
+
 import {AxiosError} from "axios";
+import {useState} from "react";
 
 type Return = {
     fetching: (...args: any[]) => Promise<void>,
     isLoading: boolean,
-    error: string
+    error: string,
+    clearError?: () => void
 }
 
 export const useFetch = (callback: Function): Return => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
 
+    const clearError = () => {
+        setError('')
+    }
+
     const fetching = async (...args: any[]) => {
         try {
+            clearError()
             setIsLoading(true)
             await callback(...args)
         } catch (_e) {
@@ -20,15 +27,9 @@ export const useFetch = (callback: Function): Return => {
             // @ts-ignore
             setError(e.response?.data?.message);
         } finally {
-            setTimeout(() => {
-                setIsLoading(false)
-            }, 200);
-
-            setTimeout(() => {
-                setError('')
-            }, 1000)
+            setIsLoading(false)
         }
     }
 
-    return {fetching, isLoading, error}
+    return {fetching, isLoading, error, clearError}
 }
