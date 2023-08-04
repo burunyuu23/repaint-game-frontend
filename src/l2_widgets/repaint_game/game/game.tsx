f698655d3301a0e622afb9c90964b378b4665fb6
danilkk <ezzfvkoko@gmail.com>
2023-08-04 18:11:19 +0300

    refactor: <Game/> to <GamePanel/>

--- src/l2_widgets/repaint_game/game/game.tsx
+++ src/l2_widgets/repaint_game/game/game.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {GameStartResponseDTO} from "@/l4_entities/repaint-game/dtos/responses/gameStartResponseDTO";
 import Map from "@/l3_features/repaint_game/map/map";
 import ColorButton from "@/l3_features/repaint_game/color_button/colorButton";
@@ -9,41 +9,52 @@ import styled from "@emotion/styled";
 import {NonRatingNoAuthService} from "@/l4_entities/repaint-game/nonrating-noauth-service/service";
 import {GameStepResponseDTO} from "@/l4_entities/repaint-game/dtos/responses/gameStepResponseDTO";
 import {useFetch} from "@/l5_shared/hooks/useFetch";
-import { Alert } from '@mui/material';
 import FixedErrorAlert from "@/l3_features/fixed_error_alert/fixedErrorAlert";
 import EndGamePanel from "@/l3_features/repaint_game/end_game_panel/endGamePanel";
-import Timer from "@/l3_features/timer/timer";
+import GameInfoPanel from "@/l2_widgets/repaint_game/game/game_info_panel/gameInfoPanel";
+import {Cell} from "@/l4_entities/repaint-game/models/cell";
 
-const Game = () => {
+const Game = React.memo(() => {
     let [data, setData] =
         useState<GameStepResponseDTO | null>(null);
 
-    useEffect(() => {
-        fetchStartGame().then(r => r);
-    }, [])
+    const [prevCapturedCount, setPrevCapturedCount]
+        = useState<number>(0);
+
+    const [prevMap, setPrevMap] = useState<Cell[][] | null>(null);
 
     const {
         fetching: fetchStartGame,
         isLoading: isStartGameLoading,
         error: startGameError
-    } = useFetch(async () => {
+    } = useFetch(useCallback(async () => {
         const startResponseDTO = await NonRatingNoAuthService.startGame(
             {
                 paletteId: 0,
-                fieldSize: 12,
+                fieldSize: 22,
                 maxRounds: 22,
             }) as GameStartResponseDTO & { currentRound: number; end: boolean; stepTime: Date };
         startResponseDTO.currentRound = 0;
         startResponseDTO.end = false;
         startResponseDTO.stepTime = startResponseDTO.startTime;
         setData(startResponseDTO);
-    })
+        setPrevMap(startResponseDTO.map);
+        setPrevCapturedCount(startResponseDTO.capturedCount)
+    }, []));
+
+    useEffect(() => {
+        fetchStartGame();
+    }, [])
 
     const {
         fetching: fetchStepGame,
         isLoading: isStepGameLoading,
-        error: stepGameError
+        error: stepGameError,
+        clearError: clearError
     } = useFetch(async (id: number) => {
+        setPrevCapturedCount(data!.capturedCount)
+        setPrevMap(data!.map);
+
         const stepResponseDTO = await NonRatingNoAuthService.stepGame(
             {
                 gameId: data!.gameId,
@@ -55,34 +66,37 @@ const Game = () => {
     })
 
 
-    const mapSize = 75;
+    const mapSize = 60;
     const buttonPanelGap = 10;
-    const mapSizeStyle = (divider: number) => `min(${mapSize/divider}dvh, ${mapSize/divider}dvw)`;
-    const mapSizeDefaultStyle = `${mapSizeStyle(1)}`;
+    const mapSizeDivider = 1;
+    const mapSizeStyle = useCallback((divider: number) =>
+        `min(${mapSize / divider}dvh, ${1.5 * mapSize / divider}dvw)`, [mapSize]);
+    const mapSizeDefaultStyle = `${mapSizeStyle(mapSizeDivider)}`;
+    const size = `calc(${mapSizeStyle(6 * mapSizeDivider)} - ${buttonPanelGap}px)`;
 
-    const Game = styled.div`
+    const GamePanel = styled.div`
       grid-template-columns: repeat(${data?.fieldSize}, 1fr);
+      height: 90dvh;
     `
 
     const ButtonPanel = styled.div`
       width: ${mapSizeDefaultStyle};
-      height:  calc(${mapSizeStyle(6)} - ${buttonPanelGap}px);
+      height: ${size};
       column-gap: ${buttonPanelGap}px;
     `
 
     return (
-        <Game>
-            {data !== null && <Timer startTime={data.startTime}/>}
-
-            {data !== null    && <EndGamePanel
-                currentRound={0}
-                maxRounds={22}
-                win={false}
-                startTime={data!.startTime}
+        <GamePanel>
+            {data !== null && <GameInfoPanel
+                prevCapturedCount={prevCapturedCount}
+                size={size}
+                data={data}
             />}
 
             {stepGameError !== '' &&
-                (<FixedErrorAlert errorMessage={stepGameError} closable />)}
+                (<FixedErrorAlert errorMessage={stepGameError}
+                                  onclose={() => clearError !== undefined ? clearError() : {}}
+                                  closable/>)}
 
             {data?.end === true &&
                 (<EndGamePanel
@@ -94,13 +108,14 @@ const Game = () => {
 
 
             {data !== null && <div>
-                <h1>{data.currentRound}/{data.maxRounds}</h1>
-                    <Map map={data.map}
-                                        colors={data!.colors}
-                                        onclick={() => {}}
-                                        fieldSize={data.fieldSize}
-                                        mapSize={mapSizeDefaultStyle}
-            />
+                <Map map={data.map}
+                     prevMap={prevMap!}
+                     colors={data!.colors}
+                     onclick={() => {
+                     }}
+                     fieldSize={data.fieldSize}
+                     mapSize={mapSizeDefaultStyle}
+                />
 
                 <ButtonPanel className={styles.buttonPanel}>
                     {data.colors.map(color =>
@@ -111,8 +126,8 @@ const Game = () => {
                         />)}
                 </ButtonPanel></div>
             }
-        </Game>
+        </GamePanel>
     );
-};
+});
 
 export default Game;
