ffbffc5ded9b7e7de97de834e16a62d2900fe1b1
danilkk <ezzfvkoko@gmail.com>
2023-08-06 14:55:26 +0300

    refactor: change modalPanel style performance

--- src/l5_shared/lib/modal_panel/modalPanel.tsx
+++ src/l5_shared/lib/modal_panel/modalPanel.tsx
@@ -5,21 +5,25 @@ import React from "react";
 
 type Props = {
     children: React.ReactNode,
-    zIndex?: number
+    zIndex?: number,
+    bg?: string,
+    className?: string
 }
 
-const ModalPanel = ({children, zIndex}: Props) => {
+const ModalPanel = ({children, zIndex, bg, className}: Props) => {
 
     const baseZIndex = zIndex ? zIndex : 999;
+    const baseBg = bg ? bg : "black"
+    const baseClassname = className ? className : ""
 
     return (
         <div className={styles.background}
-             style={{zIndex: baseZIndex - 1}} >
+             style={{zIndex: baseZIndex - 1, background: baseBg}} >
             <Modal
                 className={styles.modal}
-                style={{zIndex: baseZIndex}}
+                style={{zIndex: baseZIndex, background: baseBg}}
                 open={true}>
-                <Box className={styles.main}>
+                <Box className={[styles.main, baseClassname].join(' ')}>
                     {children}
                 </Box>
             </Modal>
