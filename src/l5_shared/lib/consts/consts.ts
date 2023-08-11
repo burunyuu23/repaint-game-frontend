137d05bc7bd3b68f4a29a40548370a60a796742a
danilkk <ezzfvkoko@gmail.com>
2023-08-11 19:23:55 +0300

    feat: add consts for logic

---
+++ src/l5_shared/lib/consts/consts.ts
@@ -0,0 +1,7 @@
+export const fieldSizeMin = 2;
+export const fieldSizeDefault = 12;
+export const fieldSizeMax = 30;
+
+export const maxRoundsMin = 1;
+export const maxRoundsDefault = 22;
+export const maxRoundsMax = 100;
