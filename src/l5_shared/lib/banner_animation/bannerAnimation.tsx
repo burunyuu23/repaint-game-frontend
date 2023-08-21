af559ea3c90544e643726affa421598ca99306dc
danilkk <ezzfvkoko@gmail.com>
2023-08-21 17:17:39 +0300

    refactor: extract 3d banner animations into shared component

--- src/l5_shared/lib/banner_animation/bannerAnimation.tsx
+++ src/l5_shared/lib/banner_animation/bannerAnimation.tsx
@@ -1,16 +1,21 @@
-import React, {useRef, useEffect, useState} from 'react';
-import { useFrame } from '@react-three/fiber';
-import {Clone, useGLTF } from '@react-three/drei';
+import React, {useEffect, useRef, useState} from 'react';
+import {useFrame} from '@react-three/fiber';
+import {Clone, useGLTF} from '@react-three/drei';
 import {AnimationAction, AnimationMixer, Euler, Group} from "three";
 
-const RepaintGameBannerAnimation = React.memo(() => {
+type Props = {
+    path: string,
+    startFrame?: number,
+    rotation?: Euler,
+    repeatTime: number
+}
+
+const BannerAnimation = React.memo(({path, startFrame, rotation, repeatTime}: Props) => {
     const groupRef = useRef<Group>(null);
 
     // Загружаем .gltf модель
-    const { scene, animations } = useGLTF('/repaint_game_banner/RepaintShinigami.gltf');
-
+    const {scene, animations} = useGLTF(path);
     console.log(scene)
-
     console.log(animations)
 
     // Переменная для управления временем анимации
@@ -18,59 +23,61 @@ const RepaintGameBannerAnimation = React.memo(() => {
     const actionsRef = useRef<AnimationAction[]>([]);
 
     // Стартовый кадр, с которого начнется анимация
-    const startFrame = 0; // Измените на нужный
+    const _startFrame = startFrame ?
+        startFrame > 0 ?
+            startFrame : 0
+        : 0; // Измените на нужный
+
+    const _rotation = rotation ? rotation : new Euler(0, 5 * Math.PI / 3, 11 * Math.PI / 6, 'XYZ')
 
     const [animationFinished, setAnimationFinished] = useState(false);
 
     // Инициализация анимации
     useEffect(() => {
-        console.log('hi!')
-
         if (groupRef.current !== null) {
             mixerRef.current = new AnimationMixer(groupRef.current);
+            console.log(mixerRef.current)
 
             actionsRef.current = animations.map((animation) => {
                 const action = mixerRef.current!.clipAction(animation);
                 action.repetitions = 1;
 
-                action.time = startFrame * animation.tracks[0].times[animation.tracks[0].times.length - 1] / 100;
+                action.time = _startFrame * animation.tracks[0].times[animation.tracks[0].times.length - 1] / 100;
+
                 action.play();
                 action.enabled = true;
 
                 console.log(action)
+
                 return action;
             });
         }
-    }, [animations, startFrame, animationFinished]);
+    }, [animations, _startFrame, animationFinished]);
 
     // Обновление анимации на каждом кадре
     useFrame((_, delta) => {
-        console.log('frame!')
-
         // Проверяем, достигла ли анимация последнего кадра
         const animationFinished = actionsRef.current.every(
             (action) => action.time >= 99 * action.getClip().tracks[0].times[action.getClip().tracks[0].times.length - 1] / 100
         );
 
         if (animationFinished) {
-            console.log('finished!')
             actionsRef.current.forEach((action) => {
                 // Устанавливаем время анимации на предпоследний кадр перед достижением конца
                 action.enabled = true;
-                action.time = 85 * action.getClip().tracks[0].times[action.getClip().tracks[0].times.length - 1] / 100;
+                action.time = (repeatTime > 0 ? repeatTime : 0) * action.getClip().tracks[0].times[action.getClip().tracks[0].times.length - 1] / 100;
             });
-        }
-        else {
+        } else {
             mixerRef.current?.update(delta);
         }
     });
 
     return (
         <group ref={groupRef}
-               rotation={new Euler(0,5*Math.PI / 3,11*Math.PI / 6,'XYZ')}>
-            <Clone object={scene} />
+               rotation={_rotation}>
+            <Clone object={scene}/>
         </group>
     );
 });
 
-export default RepaintGameBannerAnimation;
+export default BannerAnimation;
