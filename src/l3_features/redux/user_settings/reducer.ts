a8c810f8bc8aa551b468f9b7aec513437c1c0164
danilkk <ezzfvkoko@gmail.com>
2023-08-24 17:44:38 +0300

    feat: login/reg open store

--- src/l3_features/redux/user_settings/reducer.ts
+++ src/l3_features/redux/user_settings/reducer.ts
@@ -1,15 +1,30 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 export type UserSettings = {
+    isAuth: boolean,
+    isLoginOpen: boolean,
+    isRegisterOpen: boolean
 }
 
 export const initialState: UserSettings = {
+    isAuth: false,
+    isLoginOpen: false,
+    isRegisterOpen: false
 }
 
 export const UserSettingsSlice = createSlice({
     name: 'user__settings',
     initialState,
     reducers: {
+        UpdateIsAuth(state, action: PayloadAction<boolean>) {
+            state.isAuth = action.payload;
+        },
+        UpdateIsLoginOpen(state, action: PayloadAction<boolean>) {
+            state.isLoginOpen = action.payload;
+        },
+        UpdateIsRegisterOpen(state, action: PayloadAction<boolean>) {
+            state.isRegisterOpen = action.payload;
+        },
     }
 })
 
