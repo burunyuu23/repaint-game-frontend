ddff335453342c0a900d89a4db6b77c54460122f
danilkk <ezzfvkoko@gmail.com>
2023-08-27 21:39:48 +0300

    feat: auth and no auth dnlkkHubBanner.tsx

--- src/l3_features/banners/dnlkkhub_banner/dnlkkHubBanner.tsx
+++ src/l3_features/banners/dnlkkhub_banner/dnlkkHubBanner.tsx
@@ -7,11 +7,13 @@ import CarouselPaper from "@/l5_shared/lib/carousel_paper/carouselPaper";
 import BannerAnimation from "@/l5_shared/lib/banner_animation/bannerAnimation";
 import {Euler} from "three";
 import styles from "./dnlkkHubBanner.module.scss";
-import { Button } from '@mui/material';
+import {Button} from '@mui/material';
 import styled from 'styled-components';
 import {devices} from "@/l5_shared/consts/css/display_size";
 import {useAppDispatch} from "@/l5_shared/hooks/useAppDispatch";
 import UserSettingsSlice from "@/l3_features/redux/user_settings/reducer";
+import {useAppSelector} from "@/l5_shared/hooks/useAppSelector";
+import { cookie_token_clear } from '@/l5_shared/util/cookie_worker';
 
 type Props = {
     setter: () => void
@@ -21,16 +23,17 @@ const DnlkkHubBanner = ({setter}: Props) => {
 
     const Logo = styled.div`
       font-size: 16px;
-        
+
       @media ${devices.mobileM} {
         font-size: 24px;
       }
-      
+
       @media ${devices.tablet} {
         font-size: 36px;
       }
     `
 
+    const isAuth = useAppSelector(state => state.user__settings.isAuth)
     const dispatch = useAppDispatch();
 
     return (
@@ -56,19 +59,34 @@ const DnlkkHubBanner = ({setter}: Props) => {
                     <Logo>
                         Welcome to the DnlkkHub!
                     </Logo>
-                    <div className={styles.loginPanel}>
-                        <Button onClick={() => dispatch(UserSettingsSlice.actions.UpdateIsLoginOpen(true))}>Login</Button>
-                        <Button onClick={() => dispatch(UserSettingsSlice.actions.UpdateIsRegisterOpen(true))}>Register</Button>
-                    </div>
+                    {!isAuth &&
+                        <div className={styles.welcomePanel}>
+                            <Button onClick={() => dispatch(UserSettingsSlice.actions.UpdateIsLoginOpen(true))}>
+                                Login
+                            </Button>
+                            <Button onClick={() => dispatch(UserSettingsSlice.actions.UpdateIsRegisterOpen(true))}>
+                                Register
+                            </Button>
+                        </div>}
+                    {isAuth &&
+                        <div className={styles.welcomePanel}>
+                            <Button>Profile</Button>
+                            <Button onClick={() => {
+                                cookie_token_clear()
+                                dispatch(UserSettingsSlice.actions.UpdateIsAuth(false))
+                            }}>
+                                Logout
+                            </Button>
+                        </div>
+                    }
                     {/*<div className={styles.loginPanel}>*/}
-                    {/*    <Button>Profile</Button>*/}
-                    {/*    <Button>Logout</Button>*/}
                     {/*</div>*/}
                 </div>
             </div>
 
         </CarouselPaper>
-    );
+    )
+        ;
 };
 
 export default DnlkkHubBanner;
