9e18070240835d200cab7552c35958db1070097a
danilkk <ezzfvkoko@gmail.com>
2023-08-27 21:39:09 +0300

    refactor: panels.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import styles from './page.module.scss'
 import MainBanner from "@/l2_widgets/main_banner/mainBanner";
-import LoginPanel from "@/l2_widgets/login_panel/loginPanel";
-import RegisterPanel from "@/l2_widgets/register_panel/registerPanel";
+import React from "react";
+import Panels from "./panels";
 
 export default function Home() {
   return (
     <div className={styles.main}>
 
-      <LoginPanel />
-      <RegisterPanel />
+      <Panels />
 
         <MainBanner />
 
@@ -16,46 +15,6 @@ export default function Home() {
       <ul>
         <li>First</li>
         <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
-          <li>First</li>
-          <li>Second</li>
       </ul>
     </div>
   )
