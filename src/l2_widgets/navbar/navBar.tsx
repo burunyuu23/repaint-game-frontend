262893e46644fe4ccea7d4d1af1598588dace95b
danilkk <ezzfvkoko@gmail.com>
2023-08-27 21:40:34 +0300

    feat: navBar onMount load access token from cookie

--- src/l2_widgets/navbar/navBar.tsx
+++ src/l2_widgets/navbar/navBar.tsx
@@ -8,15 +8,30 @@ import WidgetsIcon from '@mui/icons-material/Widgets';
 import {Button} from "@mui/material";
 import {defaultColors} from "@/l5_shared/consts/repaint_game_settings";
 import AsideNavBar from "./aside_nav_bar/asideNavBar";
+import {cookie_get_access_token, cookie_get_refresh_token, cookie_set_token} from "@/l5_shared/util/cookie_worker";
+import {useAppDispatch} from "@/l5_shared/hooks/useAppDispatch";
+import UserSettingsSlice from "@/l3_features/redux/user_settings/reducer";
+import {AuthService} from "@/l4_entities/user/auth-service/service";
 
 const NavBar = () => {
     const [baseColors, setBaseColors] = useState<string[]>([])
     const [entered, setEntered] = useState<boolean>(false);
 
+    const dispatch = useAppDispatch();
     useEffect(() => {
         fetchColors();
-    }, [])
 
+        if (cookie_get_access_token())
+            dispatch(UserSettingsSlice.actions.UpdateIsAuth(true))
+        else {
+            const refresh_token = cookie_get_refresh_token()
+            if (refresh_token) {
+                AuthService.refreshToken({refresh_token})
+                    .then(resp => cookie_set_token(resp))
+                dispatch(UserSettingsSlice.actions.UpdateIsAuth(true))
+            }
+        }
+    }, [])
 
     const fetchColors = () => {
         PaletteService.getBasePalette()
