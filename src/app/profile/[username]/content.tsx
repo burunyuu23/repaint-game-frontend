89c18c8fbb66d77bb2e6d444454f1a6c76501f4b
danilkk <ezzfvkoko@gmail.com>
2023-08-28 19:20:36 +0300

    feat: init user profile page

--- src/app/profile/[username]/content.tsx
+++ src/app/profile/[username]/content.tsx
@@ -2,15 +2,33 @@
 
 import React from 'react';
 import {UserProfile} from "@/l4_entities/user/models/user";
+import styles from "./content.module.scss"
 
 type Props = {
     profile: UserProfile
 }
 
 const Content = ({profile}: Props) => {
+    console.log(profile)
+    const from = (new Date(profile.createdTimestamp)).toDateString();
 
     return (
-                <div>{profile.username}</div>
+        <div className={styles.main}>
+            <header className={styles.header}>
+                <div className={styles.mainInfo}>
+                    <div style={{backgroundImage: `url(${profile.image_url})`}}
+                         className={styles.banner}>
+                        <i>from {from}</i>
+                    </div>
+
+                    <b>{profile.username}</b>
+                    <img src={profile.image_url}
+                         alt="profile_image"/>
+                    <text>{profile.first_name} {profile.last_name}</text>
+                    <i>{profile.birthdate}</i>
+                </div>
+            </header>
+        </div>
     );
 };
 
