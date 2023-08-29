2407813c444d87de031ffe589714a2057f647212
danilkk <ezzfvkoko@gmail.com>
2023-08-29 20:10:43 +0300

    feat: isMe, edit me and add friend

--- src/app/profile/[username]/content.tsx
+++ src/app/profile/[username]/content.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from 'react';
+import React, {useState} from 'react';
 import {UserProfile} from "@/l4_entities/user/models/user";
 import styles from "./content.module.scss"
 import {Button} from "@mui/material";
 import styled from "styled-components";
+import {get_is_token_active} from "@/l5_shared/util/cookie_worker";
+import {UsersPostsService} from "@/l4_entities/user/users-posts-service/service";
 
 type Props = {
     profile: UserProfile
@@ -14,9 +16,28 @@ const ProfileBanner = styled.div`
   border-bottom: 2px solid #ffdf3c;
   border-top: 2px solid #ffdf3c;
 `
+async function getUserProfile() {
+    if (!get_is_token_active())
+        throw new Error();
+
+    return await UsersPostsService.profile()
+}
 
 const Content = ({profile}: Props) => {
     const from = (new Date(profile.createdTimestamp)).toDateString();
+
+    const [isMe, setIsMe]= useState<boolean>(false);
+        getUserProfile()
+            .then(resp => {
+                if (resp.username === profile.username) {
+                    setIsMe(true)
+                }
+            })
+            .catch(e => setIsMe(false));
+
+        const handleProfileButton = () => {
+
+        }
 
     return (
         <div className={styles.main}>
@@ -30,8 +51,9 @@ const Content = ({profile}: Props) => {
                     <b className={styles.username}>{profile.username}</b>
                     <div style={{backgroundImage: `url(${profile.profile_image_url})`}}
                          className={[styles.profileImage, styles.dropShadow].join(' ')}>
-                        <Button className={[styles.editButton, styles.dropShadow].join(" ")}>
-                            edit profile
+                        <Button className={[styles.profileButton, styles.dropShadow].join(" ")}
+                        onClick={handleProfileButton}>
+                            {isMe ? 'edit profile' : 'add friend'}
                         </Button>
                     </div>
                     <p>{profile.first_name} {profile.last_name}</p>
