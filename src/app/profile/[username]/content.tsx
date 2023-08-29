83883e91bc5953c869bbba0608447868a09cb203
danilkk <ezzfvkoko@gmail.com>
2023-08-29 14:44:44 +0300

    fix: banner naturalWidth 1min animation

--- src/app/profile/[username]/content.tsx
+++ src/app/profile/[username]/content.tsx
@@ -1,29 +1,51 @@
 "use client";
 
-import React from 'react';
+import React, {useState} from 'react';
 import {UserProfile} from "@/l4_entities/user/models/user";
 import styles from "./content.module.scss"
 import {Button} from "@mui/material";
+import styled from "styled-components";
+import { getImage } from '@/l5_shared/util/image';
 
 type Props = {
     profile: UserProfile
 }
 
 const Content = ({profile}: Props) => {
-    console.log(profile)
     const from = (new Date(profile.createdTimestamp)).toDateString();
+
+    const [bannerWidth, setBannerWidth] = useState<number>(0);
+
+    // TODO: -error ReferenceError: Image is not defined
+    getImage(profile.banner_image_url, (err: string | Event, img: HTMLImageElement) => {
+        setBannerWidth(img.naturalWidth);
+    });
+
+    const Banner = styled.div`
+      background-image: url(${profile.banner_image_url});
+
+      animation: slider 60s linear infinite;
+
+      @keyframes slider {
+        from {
+          background-position: 0 0;
+        }
+        to {
+          background-position: ${bannerWidth}px 0;
+        }
+      }
+    `
 
     return (
         <div className={styles.main}>
             <header className={styles.header}>
                 <div className={styles.mainInfo}>
-                    <div style={{backgroundImage: `url(${profile.image_url})`}}
-                         className={styles.banner}>
+                    <Banner className={styles.banner}>
                         <i>from {from}</i>
-                    </div>
+                    </Banner>
 
                     <b>{profile.username}</b>
-                    <div style={{backgroundImage: `url(${profile.image_url})`}}
+                    <div style={{backgroundImage: `url(${profile.profile_image_url})`}}
                          className={styles.profileImage}>
                         <Button className={styles.editButton}>
                             edit profile
---
+++ src/l5_shared/util/image.ts
@@ -0,0 +1,6 @@
+export const getImage = (url: string, cb: Function) => {
+    const img = new Image();
+    img.onload = () => cb(null, img);
+    img.onerror = (err) => cb(err);
+    img.src = url;
+};
