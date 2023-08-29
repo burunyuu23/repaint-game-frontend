f5c339078628bba56a4d7fd6a9b593ddfc1cd19a
danilkk <ezzfvkoko@gmail.com>
2023-08-29 13:10:20 +0300

    feat: add edit button

--- src/app/profile/[username]/content.tsx
+++ src/app/profile/[username]/content.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import {UserProfile} from "@/l4_entities/user/models/user";
 import styles from "./content.module.scss"
+import {Button} from "@mui/material";
 
 type Props = {
     profile: UserProfile
@@ -22,10 +23,14 @@ const Content = ({profile}: Props) => {
                     </div>
 
                     <b>{profile.username}</b>
-                    <img src={profile.image_url}
-                         alt="profile_image"/>
-                    <text>{profile.first_name} {profile.last_name}</text>
-                    <i>{profile.birthdate}</i>
+                    <div style={{backgroundImage: `url(${profile.image_url})`}}
+                         className={styles.profileImage}>
+                        <Button className={styles.editButton}>
+                            edit profile
+                        </Button>
+                    </div>
+                    <p>{profile.first_name} {profile.last_name}</p>
+                    <p>{profile.birthdate}</p>
                 </div>
             </header>
         </div>
