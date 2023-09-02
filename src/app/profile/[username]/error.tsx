8eb339156703d25f51629f79deefeb672ecee043
danilkk <ezzfvkoko@gmail.com>
2023-09-02 21:09:43 +0300

    feat: add error page for username doesn't exists

---
+++ src/app/profile/[username]/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <main style={{display: "flex", width:"100dvw", justifyContent: "center"}}>
+           User with this username doesn't exists!
+        </main>
+    );
+};
+
+export default Error;
