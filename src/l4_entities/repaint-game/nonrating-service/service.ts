83a6dc23019b93f5ea43a02eba2cb4331004fbb1
danilkk <ezzfvkoko@gmail.com>
2023-09-04 00:39:08 +0300

    feat: add userGames

--- src/l4_entities/repaint-game/nonrating-service/service.ts
+++ src/l4_entities/repaint-game/nonrating-service/service.ts
@@ -1,10 +1,13 @@
 import {GameStartResponseDTO} from "@/l4_entities/repaint-game/dtos/responses/gameStartResponseDTO";
 import {GameStartRequestDTO} from "@/l4_entities/repaint-game/dtos/requests/gameStartRequestDTO";
-import {startUrl, stepUrl} from "@/l5_shared/api/repaint_game/nonRatingGame";
+import {startUrl, stepUrl, userGamesUrl} from "@/l5_shared/api/repaint_game/nonRatingGame";
 import api from "@/l5_shared/api/axios";
 import {GameStepRequestDTO} from "@/l4_entities/repaint-game/dtos/requests/gameStepRequestDTO";
 import {GameStepResponseDTO} from "@/l4_entities/repaint-game/dtos/responses/gameStepResponseDTO";
 import {cookie_get_access_token} from "@/l5_shared/util/cookie_worker";
+import {Page, RequestParam} from "@/l5_shared/types/requestParam";
+import {NonRatingGame} from "@/l4_entities/repaint-game/models/game";
+import {request_params_create} from "@/l5_shared/util/request_params_creater";
 
 export class NonRatingAuthService {
 
@@ -27,6 +30,12 @@ export class NonRatingAuthService {
                 "Authorization": "Bearer " + jwt
             }
         })
+            .then(resp => resp.data)
+        return response;
+    }
+
+    static userGames = async (requestParam: RequestParam, userId: string): Promise<Page<NonRatingGame>> => {
+        const response: Page<NonRatingGame> = await api.get(`${userGamesUrl(userId)}?${request_params_create(requestParam)}`)
             .then(resp => resp.data)
         return response;
     }
--- src/l5_shared/api/repaint_game/nonRatingGame.ts
+++ src/l5_shared/api/repaint_game/nonRatingGame.ts
@@ -4,7 +4,11 @@ const requestMapping = "/game"
 
 const startMapping = "/start"
 const stepMapping = "/step"
+const userGamesMapping = "/user"
 const getGameUrl = (gameId: string) => `/${gameId}`
+const getUserGamesUrl = (userId: string) => `${userGamesMapping}/${userId}`
 
 export const startUrl = `${repaintGameUrl}${requestMapping}${startMapping}`
 export const stepUrl = `${repaintGameUrl}${requestMapping}${stepMapping}`
+
+export const userGamesUrl = (userId: string) => `${repaintGameUrl}${requestMapping}${getUserGamesUrl(userId)}`
