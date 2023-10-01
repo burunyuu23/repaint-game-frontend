68627aba38099ba1357e55b718f1cd3171e531e8
danilkk <ezzfvkoko@gmail.com>
2023-10-01 16:51:37 +0300

    feat: add dateTime converter

--- src/l5_shared/util/date.ts
+++ src/l5_shared/util/date.ts
@@ -2,6 +2,23 @@ export const formatDate = (date: Date) => {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
     const year = date.getFullYear();
-
+    
     return `${year}-${month}-${day}`;
+}
+
+export const formatDateTime = (date: Date) => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const secs = date.getSeconds();
+
+    return `${formatDate(date)}~${hours}:${minutes}:${secs}`;
+}
+
+export const timeRange = (from: Date, to: Date) => {
+    const toTime = (new Date(to)).getTime();
+    const fromTime = (new Date(from)).getTime();
+
+    let range = (toTime - fromTime)/1000;
+    
+    return range.toFixed(2) + "c";
 }
